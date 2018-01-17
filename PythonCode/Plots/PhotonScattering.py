0a5ab780e3d09aab55811168e3d7a0bd5111e0be
Zach Greene <zsg2105@columbia.edu>
2018-01-17 12:06:17 -0500

    update before computer hard drive reset

---
+++ PythonCode/Plots/PhotonScattering.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('/Users/Zach/XENON/Thesis/Chapter2/data/PhotonCrossSection.csv', delimiter=' ')
+
+columnsToPlot= [
+		'CoherentScat',
+		'IncoherScatt',
+		'PhotoelAbsorb',
+		'NuclearPr',
+		'ElectronPrd',
+		'TotwCoherent',
+		'TotwoCoherent'
+		]
+
+#colorsToPlot = {
+#		'CoherentScat': ,
+#		'IncoherScatt': ,
+#		'PhotoelAbsorb': ,
+#		'NuclearPr': ,
+#		'ElectronPrd': ,
+#		'TotwCoherent': ,
+#		'TotwoCoherent'
+#		}
+
+fig, ax = plt.subplots(1, 1, figsize=(16, 12))
+
+ax.plot(
+	df.PhotonEnergy,
+	df.PhotoelAbsorb,
+	color='r',
+	linewidth=3,
+	linestyle='dashed',
+	label='Photoelectric Absorption'
+)
+
+ax.plot(
+	df.PhotonEnergy,
+	df.IncoherScatt,
+	color='g',
+	linewidth=3,
+	linestyle='dashed',
+	dashes=(10, 4),
+	label='Compton Scattering'
+)
+
+ax.plot(
+	df.PhotonEnergy,
+	df.ElectronPrd,
+#	color='fuchsia',
+	color='b',
+	linewidth=3,
+	linestyle='dashed',
+	dashes=(4, 3),
+	label='Pair Production'
+)
+
+
+ax.plot(
+	df.PhotonEnergy,
+	df.TotwCoherent,
+	color='k',
+	linewidth=3,
+	label='Total'
+)
+
+ax.set_ylim([1e-5, 1e4])
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel('Photon Energy [MeV]', size=15)
+#ax.set_ylabel('$\\rm{Mass\ Attenution\ Coefficiency\ [cm^{2}\ g^{-1}}$]', size=15)
+ax.set_ylabel('Mass Attenution Coefficiency [cm$^{2}$ g$^{-1}$]', size=15)
+ax.legend(fontsize=25)
+plt.savefig('/Users/Zach/XENON/Thesis/Chapter2/Figures/PhotonAttenuation.png', format='png')
+plt.show()
---
+++ PythonCode/Plots/StoppingPower.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['text.latex.unicode'] = True
+import matplotlib.pyplot as plt
+
+PathToData = '/Users/Zach/XENON/Thesis/Chapter2/data/'
+Alphas = pd.read_csv(PathToData + 'AlphaMassStoppingPower.csv', delimiter=' ', header=3)
+Electrons = pd.read_csv(PathToData + 'ElectronMassStoppingPower.csv', delimiter=' ', header=3)
+Protons = pd.read_csv(PathToData + 'ProtonMassStoppingPower.csv', delimiter=' ', header=3)
+
+PlotColors = {
+		'alpha': 'b',
+		'electron': 'g',
+		'proton': 'fuchsia'
+	}
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+
+ax.plot(
+	Alphas.KineticEnergy,
+	Alphas.ElectronStopPow,
+	linewidth=2,
+#	linestyle='-.',
+#	label='$\\alpha$',
+	color=PlotColors['alpha']
+)
+
+
+ax.plot(
+	Electrons.KineticEnergy,
+	Electrons.ElectronStopPow,
+	linewidth=2,
+	linestyle='dashed',
+#	label='$e^{-}$'
+	color=PlotColors['electron']
+)
+
+ax.plot(
+	Protons.KineticEnergy,
+	Protons.ElectronStopPow,
+	linewidth=2,
+	linestyle='-.',
+	dashes=(10,3),
+#	label='$\\rm{p}$'
+	color=PlotColors['proton']
+)
+
+ax.text(
+	1e-2,
+	50,
+	r'$\boldsymbol{\alpha}$',
+	color=PlotColors['alpha'],
+	size='18'
+)
+
+ax.text(
+	6e-2,
+	4,
+#	r'$\textbf{e^{-1}}$',
+	r'$e^{-1}$',
+	color=PlotColors['electron'],
+	size='16'
+)
+
+ax.text(
+	6,
+	15,
+	'p',
+	color=PlotColors['proton'],
+	size='14'
+)
+
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlim([1e-3, 1e2])
+ax.xaxis.set_tick_params(labelsize=15)
+ax.yaxis.set_tick_params(labelsize=15)
+ax.set_xlabel('Energy [MeV]')
+ax.set_ylabel('Electron Mass Stopping Power [MeV cm$^{2}$ g$^{-1}$]')
+#ax.legend()
+#plt.savefig('/Users/Zach/XENON/Thesis/Chapter2/Figures/MassStoppingPower.png', format='png')
+plt.show()

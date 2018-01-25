cb0095a8f3f965f8e574feecac651c4ed9b705b0
Zach Greene <zsg2105@columbia.edu>
2018-01-25 11:41:24 -0500

    update

--- PythonCode/Plots/PhotonScattering.py
+++ PythonCode/Plots/PhotonScattering.py
@@ -67,12 +67,13 @@ ax.plot(
 	label='Total'
 )
 
+ax.set_xlim([1e-3, 1e4])
 ax.set_ylim([1e-5, 1e4])
 ax.set_xscale('log')
 ax.set_yscale('log')
-ax.set_xlabel('Photon Energy [MeV]', size=15)
+ax.set_xlabel('Photon Energy [MeV]', size=25)
 #ax.set_ylabel('$\\rm{Mass\ Attenution\ Coefficiency\ [cm^{2}\ g^{-1}}$]', size=15)
-ax.set_ylabel('Mass Attenution Coefficiency [cm$^{2}$ g$^{-1}$]', size=15)
+ax.set_ylabel('Mass Attenution Coefficiency [cm$^{2}$ g$^{-1}$]', size=25)
 ax.legend(fontsize=25)
 plt.savefig('/Users/Zach/XENON/Thesis/Chapter2/Figures/PhotonAttenuation.png', format='png')
 plt.show()
